7b6288df5a9c974a6cc85cbb04e9129acc47178c
Vivek Kaushik <kaushikvivek.17@gmail.com>
2019-08-10 23:09:09 +0530

    Fixed assertion error

--- lib/auth/VerifyScreen.dart
+++ lib/auth/VerifyScreen.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:home_kitchen/Constant/Constant.dart';
 
-
 class VerifyScreen extends StatefulWidget {
   @override
   VerifyScreenState createState() => VerifyScreenState();
 }
 
-class VerifyScreenState extends State<VerifyScreen>{
-
+class VerifyScreenState extends State<VerifyScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Container(
-        padding: EdgeInsets.only(top: 150.0, right: 20.0, left: 20.0, bottom: 20.0),
+        padding:
+            EdgeInsets.only(top: 150.0, right: 20.0, left: 20.0, bottom: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
@@ -24,19 +23,30 @@ class VerifyScreenState extends State<VerifyScreen>{
               width: 150.0,
               height: 125.0,
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             buildTextField("Email"),
-            SizedBox(height: 10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             buildButtonContainer(),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             Container(
               child: Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text("Don't have an account?"),
-                    SizedBox(width: 10.0,),
-                    Text("SIGN UP", style: TextStyle(color: Theme.of(context).primaryColor,)),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text("SIGN UP",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        )),
                   ],
                 ),
               ),
@@ -47,7 +57,7 @@ class VerifyScreenState extends State<VerifyScreen>{
     );
   }
 
-  Widget buildTextField(String hintText){
+  Widget buildTextField(String hintText) {
     return TextField(
       decoration: InputDecoration(
         hintText: hintText,
@@ -59,44 +69,35 @@ class VerifyScreenState extends State<VerifyScreen>{
           borderRadius: BorderRadius.circular(20.0),
         ),
         prefixIcon: hintText == "Email" ? Icon(Icons.email) : Icon(Icons.lock),
-
       ),
     );
   }
 
-  Widget buildButtonContainer(){
-    GestureDetector(
-        onTap: (){
-      Navigator
-          .of(context)
-          .pushNamed(HOME_SCREEN);
-    },
-    child: new Container(
-      height: 56.0,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(23.0),
-        gradient: LinearGradient(
-            colors: [
-              Color(0xFF1DBF73),
-              Color(0xFF1DBF75)
-            ],
-            begin: Alignment.centerRight,
-            end: Alignment.centerLeft
+  Widget buildButtonContainer() {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(HOME_SCREEN);
+      },
+      child: new Container(
+        height: 56.0,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(23.0),
+          gradient: LinearGradient(
+              colors: [Color(0xFF1DBF73), Color(0xFF1DBF75)],
+              begin: Alignment.centerRight,
+              end: Alignment.centerLeft),
         ),
-      ),
-      child: Center(
-        child: Text(
-          "SUBMIT",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
+        child: Center(
+          child: Text(
+            "SUBMIT",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
           ),
         ),
       ),
-    ),
     );
-
   }
-
 }
