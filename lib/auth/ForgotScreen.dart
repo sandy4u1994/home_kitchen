06721ac228f801c4578cf0ef33ca6bd8a4be26bd
Vivek Kaushik <kaushikvivek.17@gmail.com>
2019-08-10 22:33:18 +0530

    Fixed Assertion error

--- lib/auth/ForgotScreen.dart
+++ lib/auth/ForgotScreen.dart
@@ -6,15 +6,15 @@ class ForgotScreen extends StatefulWidget {
   ForgotScreenState createState() => ForgotScreenState();
 }
 
-class ForgotScreenState extends State<ForgotScreen>{
-
+class ForgotScreenState extends State<ForgotScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Container(
-        padding: EdgeInsets.only(top: 150.0, right: 20.0, left: 20.0, bottom: 20.0),
+        padding:
+            EdgeInsets.only(top: 150.0, right: 20.0, left: 20.0, bottom: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
@@ -24,29 +24,37 @@ class ForgotScreenState extends State<ForgotScreen>{
               height: 150.0,
             ),
             buildTextField("Email"),
-            SizedBox(height: 10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             buildButtonContainer(),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             Container(
               child: Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text("Don't have an account?"),
-                    SizedBox(width: 10.0,),
-                    Text("SIGN UP", style: TextStyle(color: Theme.of(context).primaryColor,)),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text("SIGN UP",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        )),
                   ],
                 ),
               ),
             ),
-
           ],
         ),
       ),
     );
   }
 
-  Widget buildTextField(String hintText){
+  Widget buildTextField(String hintText) {
     return TextField(
       decoration: InputDecoration(
         hintText: hintText,
@@ -58,44 +66,35 @@ class ForgotScreenState extends State<ForgotScreen>{
           borderRadius: BorderRadius.circular(20.0),
         ),
         prefixIcon: Icon(Icons.phone_android, color: Colors.green),
-
       ),
     );
   }
 
-  Widget buildButtonContainer(){
-    GestureDetector(
-        onTap: (){
-      Navigator
-          .of(context)
-          .pushNamed(VERIFY_SCREEN);
-    },
-    child: new Container(
-      height: 56.0,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(23.0),
-        gradient: LinearGradient(
-            colors: [
-              Color(0xFF1DBF73),
-              Color(0xFF1DBF75)
-            ],
-            begin: Alignment.centerRight,
-            end: Alignment.centerLeft
+  Widget buildButtonContainer() {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(VERIFY_SCREEN);
+      },
+      child: new Container(
+        height: 56.0,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(23.0),
+          gradient: LinearGradient(
+              colors: [Color(0xFF1DBF73), Color(0xFF1DBF75)],
+              begin: Alignment.centerRight,
+              end: Alignment.centerLeft),
         ),
-      ),
-      child: Center(
-        child: Text(
-          "SEND OTP",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
+        child: Center(
+          child: Text(
+            "SEND OTP",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
           ),
         ),
       ),
-    ),
     );
-
   }
-
 }
